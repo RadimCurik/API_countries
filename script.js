0102ba986af1d14894582570dd590fe37a0f3e52
RadimCurik <curikradim@gmail.com>
2026-06-13 21:53:39 +0200

    Second commit

--- script.js
+++ script.js
@@ -14,12 +14,22 @@ async function loadCountries() {
         countries.forEach(country => {
             html += `
             <div class="country-card">
-                <img src="${country.flags.svg}" alt="" style="height: 50px">
-                <h2>${country.name.common}</h2>
-                <p>${country.region}</p>
-                <p>${country.population}</p>
-                <p>${country.languages}</p>
-                
+                <div class="country-name-flag">
+                    <img src="${country.flags.svg}" alt="" >
+                    <h2>${country.name.common}</h2>
+                </div> 
+                <hr/>
+                <div class="country-text"><i class='fa fa-globe'></i><p>Region: ${country.region}</p>
+                <i class="fa fa-university"></i><p>Hl. město: ${country.capital}</p></div>
+                <div class="country-text">
+                <i class='fa fa-users'></i><p>Obyvatelé: ${country.population}</p>
+                <i class='fa fa-map'></i><p>Rozloha: ${country.area} km²</p>
+                </div>
+                <div class="country-text">
+                <i class='fa fa-language'></i><p>Jazyk: ${Object.values(country.languages).join(", ")}</p>
+                <i class='fa fa-money'></i><p>Měna: ${Object.values(country.currencies).map(c => `${c.symbol} ${c.name}`).join(", ")}</p>
+                </div>
+                <div class="country-text-time"><i class='fa fa-clock-o'></i><p>Časové pásmo: ${country.timezones}</p></div>
             </div>
         `;
         })
